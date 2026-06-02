41885eb20e6d9575bbe6ad36091410e9fd644f27
aakash1612 <varshneyakash46@gmail.com>
2026-06-02 15:12:20 +0530

    Frontend chnage

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = '';
+const SOCKET_URL = import.meta.env.VITE_BACKEND_URL;
 let socket: Socket | null = null;
 
 export function getSocket(): Socket {
